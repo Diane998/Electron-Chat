5e2d60321024df85cc98974e7b166200b9b9cacc
Diane998 <gutierrez.almiradyan@gmail.com>
2021-04-20 22:46:39 +0800

    Implemented routing

--- src/js/App.js
+++ src/js/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import { HashRouter, Route, Switch } from 'react-router-dom';
+
 import Home from './views/Home';
+import NavBar from './components/NavBar';
 
 const App = () => {
   // const sendNotification = () => {
   //   electron.notificationApi.sendNotification('THis is my message');
   // };
 
-  return <Home />;
+  return (
+    <HashRouter>
+      <NavBar />
+      <div className='content-wrapper'>
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/settings' exact component={() => <h1>Settings</h1>} />
+          <Route path='/login' exact component={() => <h1>Log in</h1>} />
+          <Route path='/register' exact component={() => <h1>register</h1>} />
+        </Switch>
+      </div>
+    </HashRouter>
+  );
 };
 
 export default App;
--- src/js/views/Home.js
+++ src/js/views/Home.js
@@ -1,21 +1,17 @@
 import React from 'react';
-import NavBar from '../components/NavBar';
 import JoinedChats from '../components/JoinedChats';
 import AvailableChats from '../components/AvailableChats';
 import Title from '../components/shared/Title';
 
 const Home = () => {
   return (
-    <div className='content-wrapper'>
-      <NavBar />
-      <div className='row no-gutters fh'>
-        <div className='col-3 fh'>
-          <JoinedChats />
-        </div>
-        <div className='col-9 fh'>
-          <Title />
-          <AvailableChats />
-        </div>
+    <div className='row no-gutters fh'>
+      <div className='col-3 fh'>
+        <JoinedChats />
+      </div>
+      <div className='col-9 fh'>
+        <Title />
+        <AvailableChats />
       </div>
     </div>
   );
